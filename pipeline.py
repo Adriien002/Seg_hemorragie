b57a55a761f113d268958fd67e829f101718b67b
Adrien Tibi <adrientibi@yahoo.fr>
2025-05-08 13:53:10 -0400

    seg mbg : EDH mauvais

--- pipeline.py
+++ pipeline.py
@@ -1,156 +1,175 @@
 import os
-import glob
-import torch
-import lightning as L
-from tqdm import tqdm
-from monai.networks.nets import UNet
-from monai.losses import DiceCELoss
-from monai.metrics import DiceMetric
-from monai.data import CacheDataset, DataLoader, NibabelReader,PersistentDataset
-from monai.transforms import (
-    Compose, LoadImaged, ScaleIntensityRanged,
-    ResizeWithPadOrCropd, RandCropByPosNegLabeld, ToTensord, EnsureChannelFirstd, RandCropByLabelClassesd, AsDiscreted
-)
-from torch.optim import Adam
-from pytorch_lightning.callbacks import ModelCheckpoint
 import warnings
-from torch.utils.tensorboard import SummaryWriter
-warnings.filterwarnings("ignore", message="no available indices of class.*to crop.*")
+
+import pytorch_lightning as pl
+import torch
+from monai.data import DataLoader, PersistentDataset, Dataset
+from monai.inferers import sliding_window_inference
+from monai.losses import DiceCELoss
+from monai.metrics import DiceMetric, DiceHelper
+from monai.networks.nets import UNet
+import monai.transforms as T
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.optim import SGD
+from transformers import get_linear_schedule_with_warmup
+
 warnings.filterwarnings("ignore", message="You are using `torch.load` with `weights_only=False`")
 
 # ======================
 # 1. CONFIGURATION
 # ======================
 DATASET_DIR = '/home/tibia/Projet_Hemorragie/mbh_seg/nii'
-SAVE_DIR = "/home/tibia/Projet_Hemorragie/test/first/"
+SAVE_DIR = "/home/tibia/Projet_Hemorragie/MBH_2_log"
 os.makedirs(SAVE_DIR, exist_ok=True)
 
 # ======================
 # 2. DATA PIPELINE (same as your original)
 # ======================
-transforms = Compose([
-    LoadImaged(keys=["image", "seg"], reader=NibabelReader()),
-    EnsureChannelFirstd(keys=["image", "seg"]),
-    ScaleIntensityRanged(keys=["image"], a_min=-10, a_max=140, b_min=0.0, b_max=1.0, clip=True),
-    ResizeWithPadOrCropd(keys=["image", "seg"], spatial_size=(512, 512, 32)),
-    
-    #RandCropByLabelClassesd(
-    #keys=["image", "seg"],
-    #label_key="seg",
-    #spatial_size=(96, 96, 32),
-    #num_classes=6,    # number of foreground classes (1, 2, 3,4,5)
-    #ratios=[0, 1, 1, 1,1, 1],        # sample all classes equally
-   #num_samples=4,),
-    ToTensord(keys=["image", "seg"])
+transforms = T.Compose([
+    # Loading transforms
+    T.LoadImaged(keys=["image", "seg"]),
+    T.EnsureChannelFirstd(keys=["image", "seg"]),
+    T.CropForegroundd(keys=['image', 'seg'], source_key='image'), # who cares about the background ?
+    T.Orientationd(keys=["image", "seg"], axcodes='RAS'),  # make sure all images are the same orientation
+    T.Spacingd(keys=["image", "seg"], pixdim=(1., 1., 1.), mode=['bilinear', 'nearest']), # to isotropic spacing
+    T.SpatialPadd(keys=["image", "seg"], spatial_size=(96, 96, 96)),  # make sure we have at least 96 slices
+    T.ScaleIntensityRanged(keys=["image"], a_min=-10, a_max=140, b_min=0.0, b_max=1.0, clip=True),  # clip images
+
+    # Let's crop 2 patches per case using positive negative instead of class by class
+    #T.RandCropByPosNegLabeld(
+     #   keys=['image', 'seg'],
+      #  image_key='image',
+       # label_key='seg',
+        #pos=5.0,
+        #neg=1.0,
+        #spatial_size=(96, 96, 96),
+        #num_samples=2
+    #),
+    T.RandCropByLabelClassesd(
+    keys=["image", "seg"],
+    label_key="seg",
+    spatial_size=(96, 96, 96),  
+    num_classes=6,
+    ratios=[0.1, 0.3, 0.1, 0.1, 0.1, 0.1],  # + de poids pour les classes rares (classe 1 ici à 0.3 par ex)
+    num_samples=4, 
+    ),
+
+    # Data augmentations
+    # For intensity augmentations, small random transforms but often
+    # For spatial augmentations, only along the sagittal and coronal axis
+    T.RandScaleIntensityd(
+        keys=['image'],
+        factors=0.02,
+        prob=0.5
+    ),
+    T.RandShiftIntensityd(
+       keys=['image'],
+        offsets=0.05,
+        prob=0.5
+    ),
+    T.RandRotate90d(
+        keys=['image', 'seg'],
+        prob=0.5,
+        max_k=2,
+        spatial_axes=(0, 1)
+    ),
+    T.RandFlipd(
+        keys=['image', 'seg'],
+        prob=0.5,
+        spatial_axis=[0, 1]
+    )
 ])
+
+val_transforms = T.Compose([
+    # Loading transforms
+    T.LoadImaged(keys=["image", "seg"]),
+    T.EnsureChannelFirstd(keys=["image", "seg"]),
+    T.CropForegroundd(keys=['image', 'seg'], source_key='image'), # who cares about the background ?
+    T.Orientationd(keys=["image", "seg"], axcodes='RAS'),  # make sure all images are the same orientation
+    T.Spacingd(keys=["image", "seg"], pixdim=(1., 1., 1.), mode=['bilinear', 'nearest']), # to isotropic spacing
+    T.SpatialPadd(keys=["image", "seg"], spatial_size=(96, 96, 96)),  # make sure we have at least 96 slices
+    T.ScaleIntensityRanged(keys=["image"], a_min=-10, a_max=140, b_min=0.0, b_max=1.0, clip=True),  # clip images
+])
+
 
 def get_data_files(img_dir, seg_dir):
     images = sorted([os.path.join(img_dir, f) for f in os.listdir(img_dir) if f.endswith('.nii.gz')])
     labels = sorted([os.path.join(seg_dir, f) for f in os.listdir(seg_dir) if f.endswith('.nii.gz')])
     return [{"image": img, "seg": lbl} for img, lbl in zip(images, labels)]
 
+
 # ======================
 # 3. LIGHTNING MODULE 
 # ======================
-class HemorrhageModel(L.LightningModule):
-    def __init__(self):
+class HemorrhageModel(pl.LightningModule):
+    def __init__(self, num_steps):
         super().__init__()
+        self.num_steps = num_steps
         self.model = UNet(
             spatial_dims=3,
             in_channels=1,
             out_channels=6,
-            channels=(16, 32, 64, 128, 256),
-            strides=(2, 2, 2, 2),
+            channels=(32, 64, 128, 256, 320, 320),
+            strides=(2, 2, 2, 2, 2),
             num_res_units=2,
         )
-        self.loss_fn = DiceCELoss(include_background=False,to_onehot_y=True, softmax=True, lambda_dice=0.7, lambda_ce=0.3)
-        self.dice_metric = DiceMetric(include_background=False, reduction="mean_batch", ignore_empty=True)
-        self.best_dice = 0.0
-
-    def forward(self, x):
-        return self.model(x)
+        self.loss_fn = DiceCELoss(include_background=False, to_onehot_y=True, softmax=True) # don't need to weight the dice ce loss
+        self.dice_metric = DiceHelper(include_background=False,
+                                      softmax=True,
+                                      num_classes=6,
+                                      reduction='none')
 
     def training_step(self, batch, batch_idx):
-        x, y = batch["image"], batch["seg"]  # y is one-hot from AsDiscreted transform
-        y_logits = self(x)
-        
-        # Loss computation (automatically applies softmax)
-        loss = self.loss_fn(y_logits, y)
-       
-        # Metrics (must apply softmax first)
-        y_prob = torch.softmax(y_logits, dim=1)
-        self.dice_metric(y_prob, y)
-        
-        self.log("train_loss", loss, prog_bar=True)
-        return loss
+        x, y = batch["image"], batch["seg"]
+        y_logits = self.model(x)
 
+        loss = self.loss_fn(y_logits, y)
+
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+
+        lr = self.trainer.lr_scheduler_configs[0].scheduler.optimizer.param_groups[0]["lr"]
+        self.log("lr", lr, on_step=True, prog_bar=True)
+
+        return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch["image"], batch["seg"]
-        y_logits = self(x)
-       
-        
 
-     
+        y_hat = sliding_window_inference(x,
+                                         roi_size=(96, 96, 96),
+                                         sw_batch_size=2,
+                                         predictor=self.model)
 
-    # Loss
-        loss = self.loss_fn(y_logits, y)
+        # Loss
+        loss = self.loss_fn(y_hat, y)
 
-    # Dice computation
-        y_prob = torch.softmax(y_logits, dim=1) # (B, num_classes, H, W, D)
-       
-    # Convert y to one-hot if needed (assuming y is Bx1xHxWxD)
-        if y.shape[1] == 1:
-            y_onehot = torch.nn.functional.one_hot(y.long().squeeze(1), num_classes=6)
-            y_onehot = y_onehot.permute(0, 4, 1, 2, 3).float()
-        else:
-            y_onehot = y #(B, num_classes, H, W, D)
+        scores, _ = self.dice_metric(y_hat, y)
 
-        self.dice_metric(y_prob, y_onehot)
+        y_labels = y.unique().long().tolist()[1:]
+        scores = {label: scores[0][label - 1].item() for label in y_labels}
 
-    # Logs
-        self.log("val_loss", loss, prog_bar=True)
+        metrics = {f'dice_c{label}': score for label, score in scores.items()}
+        metrics['val_loss'] = loss
+
+        self.log_dict(metrics, on_epoch=True, prog_bar=True)
+
         return loss
 
-
-       
-    def on_train_epoch_end(self):
-        train_loss = self.trainer.callback_metrics.get("train_loss")
-        if train_loss is not None:
-            print(f"\nTrain Loss (epoch {self.current_epoch}): {train_loss.item():.4f}")
-
-    def on_validation_epoch_end(self):
-        dice_scores = self.dice_metric.aggregate()  # Shape: [num_classes]
-
-        
-        self.dice_metric.reset()
-    
-    # Log individual class scores
-        class_names = ["EDH", "IPH", "IVH","SAH","SDH"]  # Adjust to your classes
-    
-        for i, score in enumerate(dice_scores):
-        # Extract scalar value for each class
-         self.log(f"val_dice_{class_names[i]}", score.item())  # .item() converts to Python float
-    
-    # Log mean Dice (optional)
-        mean_dice = dice_scores.mean()
-        self.log("val_dice_mean", mean_dice, prog_bar=True)
-    
-        print(f"\nDice Scores:")
-        for name, score in zip(class_names, dice_scores):
-            print(f"{name}: {score.item():.4f}")
-        print(f"Mean: {mean_dice.item():.4f}")
-    
-    # Save best model
-        if mean_dice > self.best_dice:
-            self.best_dice = mean_dice
-            self.trainer.save_checkpoint(os.path.join(SAVE_DIR, "best.ckpt"))
-       
-
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=1e-3)
 
-  
+        optimizer = SGD(self.parameters(), lr=1e-3, momentum=0.99, nesterov=True, weight_decay=0.00003)
+        scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                    num_warmup_steps=0,
+                                                    num_training_steps=self.num_steps)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "frequency": 1,
+                "interval": 'step'
+            }
+        }
+
 
 # ======================
 # 4. TRAINING SETUP
@@ -158,57 +177,46 @@ class HemorrhageModel(L.LightningModule):
 
 
 def main():
+    num_epochs = 1000
     # Load data (same as original)
     train_files = get_data_files(f"{DATASET_DIR}/train/img", f"{DATASET_DIR}/train/seg")
     val_files = get_data_files(f"{DATASET_DIR}/val/img", f"{DATASET_DIR}/val/seg")
-    
+
     train_dataset = PersistentDataset(
-        train_files, 
+        train_files,
         transform=transforms,
         cache_dir=os.path.join(SAVE_DIR, "cache_train")
     )
-    
+
     val_dataset = PersistentDataset(
         val_files,
-        transform=transforms,
+        transform=val_transforms,
         cache_dir=os.path.join(SAVE_DIR, "cache_val")
     )
 
-    
-    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=8)
+    train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=8)
     val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=8)
 
     # Initialize model with checkpoint if available
-    model = HemorrhageModel()
-   
+    model = HemorrhageModel(num_steps=len(train_loader) * num_epochs)
+    print(f"Total number of steps : {len(train_loader) * num_epochs}")
 
     # Configure trainer with progress bar and checkpointing
-    trainer = L.Trainer(
-        log_every_n_steps=5,
-        max_epochs=1000,
+    trainer = pl.Trainer(
+        max_epochs=num_epochs,
+        check_val_every_n_epoch=5,
         accelerator="auto",
-        devices=[0],
+        devices=[1],
         default_root_dir=SAVE_DIR,
-        callbacks=[
-            L.pytorch.callbacks.TQDMProgressBar(refresh_rate=10),
-            L.pytorch.callbacks.ModelCheckpoint(
-            dirpath=SAVE_DIR,
-            filename="best_{epoch}_{val_dice_mean:.4f}",  
-            monitor="val_dice_mean",  # Métrique à surveiller 
-            mode="max",              # Maximiser le Dice
-            save_top_k=3,            # Garder les 3 meilleurs modèles
-            every_n_epochs=10,       # Sauvegarde périodique (optionnel)
-            save_last=True 
-            )],
-             logger=L.pytorch.loggers.TensorBoardLogger(
-        save_dir=SAVE_DIR,
-        name="lightning_logs"  # Dossier où sont stockés les logs
+        logger=TensorBoardLogger(
+            save_dir=SAVE_DIR,
+            name="lightning_logs"  # Dossier où sont stockés les logs
+        )
     )
-    )
-    
+
     # Start training
     trainer.fit(model, train_loader, val_loader)
 
+
 if __name__ == "__main__":
-    torch.cuda.empty_cache()
     main()
