7a677ee4c7a56e7e0a5d0cd432ce3fca71a97f43
Adrien Tibi <adrientibi@yahoo.fr>
2025-09-04 15:20:34 -0400

    confi

---
+++ Segmentation/config.py
@@ -0,0 +1,7 @@
+
+import os
+
+
+DATASET_DIR = '/home/tibia/Projet_Hemorragie/Seg_hemorragie/split_MONAI'
+SAVE_DIR = "/home/tibia/Projet_Hemorragie/MBH_aug_low"
+os.makedirs(SAVE_DIR, exist_ok=True)
