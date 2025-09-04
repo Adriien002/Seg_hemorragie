560a94354f96f32f9a1a1048bd3441d3ac14f7fa
Adrien Tibi <adrientibi@yahoo.fr>
2025-09-04 15:19:07 -0400

    ajout module

---
+++ Segmentation/models/lightning.py
@@ -0,0 +1,135 @@
+
+from monai.networks.nets import UNet
+from monai.inferers import sliding_window_inference
+from monai.losses import DiceCELoss
+from monai.metrics import DiceHelper
+import pytorch_lightning as pl
+from torch.optim import Adam, SGD
+from transformers import get_linear_schedule_with_warmup
+
+
+class HemorrhageModel(pl.LightningModule):
+    def __init__(self, num_steps):
+        super().__init__()
+        self.num_steps = num_steps
+        self.model = UNet(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=6,
+            channels=(32, 64, 128, 256, 320, 320),
+            strides=(2, 2, 2, 2, 2),
+            num_res_units=2,
+        )
+        # self.model = BasicUNetWithClassification(
+        #     spatial_dims=3,
+        #     in_channels=1,
+        #     out_channels=6,  # pour segmentation
+        #     num_cls_classes=6,  # pour classification
+        #     features=(32, 32, 64, 128, 256, 32),
+        #     act=("LeakyReLU", {"negative_slope": 0.1, "inplace": True}),
+        #     norm=("instance", {"affine": True}),
+        #     bias=True,
+        #     dropout=0.0,
+        #     upsample="deconv"
+        # )
+        
+        # self.loss_fn = DiceFocalLoss(
+        #     include_background=False,
+        #     to_onehot_y=True,
+        #     softmax=True,
+        #     gamma=2.0,
+        #     )
+        self.loss_fn = DiceCELoss(include_background=False, to_onehot_y=True, softmax=True) # don't need to weight the dice ce loss
+        self.dice_metric = DiceHelper(include_background=False,
+                                      softmax=True,
+                                      num_classes=6,
+                                      reduction='none')
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch["image"], batch["seg"]
+        y_logits = self.model(x)
+
+        loss = self.loss_fn(y_logits, y)
+
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+
+        lr = self.trainer.lr_scheduler_configs[0].scheduler.optimizer.param_groups[0]["lr"]
+        self.log("lr", lr, on_step=True, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch["image"], batch["seg"]
+
+        y_hat = sliding_window_inference(x,
+                                         roi_size=(96, 96, 96),
+                                         sw_batch_size=2,
+                                         predictor=self.model)
+
+        # Loss
+        loss = self.loss_fn(y_hat, y)
+
+        scores, _ = self.dice_metric(y_hat, y)
+
+        y_labels = y.unique().long().tolist()[1:]
+        scores = {label: scores[0][label - 1].item() for label in y_labels}
+
+        metrics = {f'dice_c{label}': score for label, score in scores.items()}
+        metrics['val_loss'] = loss
+
+        self.log_dict(metrics, on_epoch=True, prog_bar=True)
+
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        x, y = batch["image"], batch["seg"]
+
+        y_hat = sliding_window_inference(x,
+                                         roi_size=(96, 96, 96),
+                                         sw_batch_size=2,
+                                         predictor=self.model)
+
+        # Loss
+        loss = self.loss_fn(y_hat, y)
+
+        scores, _ = self.dice_metric(y_hat, y)
+
+        y_labels = y.unique().long().tolist()[1:]
+        scores = {label: scores[0][label - 1].item() for label in y_labels}
+
+        metrics = {f'dice_c{label}': score for label, score in scores.items()}
+        metrics['test_loss'] = loss
+
+        self.log_dict(metrics, on_epoch=True, prog_bar=True)
+
+        return loss
+
+        
+    def configure_optimizers(self):
+
+        optimizer = SGD(self.parameters(), lr=1e-3, momentum=0.99, nesterov=True, weight_decay=0.00003)
+        scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                    num_warmup_steps=0,
+                                                    num_training_steps=self.num_steps)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "frequency": 1,
+                "interval": 'step'
+            }
+        }
+        
+    # def configure_optimizers(model):
+    #     optimizer = Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
+    #     scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2, eta_min=1e-6)
+    #     return {
+    #     "optimizer": optimizer,
+    #     "lr_scheduler": {
+    #         "scheduler": scheduler,
+    #         "interval": "epoch",
+    #         "frequency": 1
+    #     }
+    # }
+
+
