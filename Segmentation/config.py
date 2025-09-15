1a07e45797ce8a253d933b71c168f8916e85d656
Adrien Tibi <adrientibi@yahoo.fr>
2025-09-15 12:28:49 -0400

    passage en dict

--- Segmentation/config.py
+++ Segmentation/config.py
@@ -2,34 +2,32 @@
 import os
 import monai.networks.nets as monai_nets
 
-#Configurations
-DATASET_DIR = '/home/tibia/Projet_Hemorragie/Seg_hemorragie/split_MONAI'
-SAVE_DIR = "/home/tibia/Projet_Hemorragie/MBH_aug_low"
-os.makedirs(SAVE_DIR, exist_ok=True)
-CHECKPOINT_PATH = os.path.join(SAVE_DIR, "best_model.ckpt")
-
-# Training parameters
-batch_size = 2
-num_epochs = 1000
-
-# Model parameters
-model = monai_nets.UNet(
-            spatial_dims=3,
-            in_channels=1,
-            out_channels=6,
-            channels=(32, 64, 128, 256, 320, 320),
-            strides=(2, 2, 2, 2, 2),
-            num_res_units=2,
-        )
-
-# Data augmentation parameters
-spatial_size = (96, 96, 96) # Taille des patches pour l'entraînement
-pos_ratio = 5.0  # Ratio pour le recadrage positif
-neg_ratio = 1.0  # Ratio pour le recadrage négatif
-
-
-#Callbacks parameters
-monitor = "val_loss"
-mode = "min"
-save_top_k = 2
-patience = 50  # Nombre d'époques sans amélioration avant d'arrêter
+CONFIG = {
+    "dataset": {
+        "dataset_dir": "/home/tibia/Projet_Hemorragie/Seg_hemorragie/split_MONAI",
+        "save_dir": "/home/tibia/Projet_Hemorragie/MBH_seg_test",
+    },
+    "training": {
+        "batch_size": 2,
+        "num_epochs": 1000,
+    },
+    "model": {
+        "spatial_dims": 3,
+        "in_channels": 1,
+        "out_channels": 6,
+        "channels": (32, 64, 128, 256, 320, 320),
+        "strides": (2, 2, 2, 2, 2),
+        "num_res_units": 2,
+    },
+    "augmentation": {
+        "spatial_size": (96, 96, 96),
+        "pos_ratio": 5.0,
+        "neg_ratio": 1.0,
+    },
+    "callbacks": {
+        "monitor": "val_loss",
+        "mode": "min",
+        "save_top_k": 2,
+        "patience": 50,
+    }
+}
